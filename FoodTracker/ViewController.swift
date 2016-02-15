ef86d5f267f2c687cdddad1b14badf29cd7b4013
Takuya OHASHI <tky.ohashi@gmail.com>
2016-02-16 00:10:59 +0900

    connect label to source code

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     // MARK: Properties
 
     @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var mealNameLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
