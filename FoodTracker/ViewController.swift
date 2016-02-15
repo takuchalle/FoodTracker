c7346e3b7c74ddb2b8b6647d4cd264a042ffbe8c
Takuya OHASHI <tky.ohashi@gmail.com>
2016-02-15 23:42:04 +0900

    connect text field to source code

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -11,7 +11,8 @@ import UIKit
 class ViewController: UIViewController {
 
     // MARK: Properties
-
+
+    @IBOutlet weak var nameTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
