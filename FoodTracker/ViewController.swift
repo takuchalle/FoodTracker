eda776673814114aa3ed8721383c010b6a88aba5
Takuya OHASHI <tky.ohashi@gmail.com>
2016-02-25 08:55:34 +0900

    implement the imagePickerController(_:didFinishPickingMediaWithInfo:) method

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -40,6 +40,14 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
         // Dismiss the picker if the user canceled
         dismissViewControllerAnimated(true, completion: nil)
     }
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        // The info dictionary contains multiple representations of the image, and this uses the original.
+        let selectedImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        // Set photoImageView to display the selected image.
+        photoImageView.image = selectedImage
+        // Dismiss the picker.
+        dismissViewControllerAnimated(true, completion: nil)
+    }
     
     @IBAction func selectImageFromPhotoLibrary(sender: UITapGestureRecognizer) {
         // Hide the keyboard
