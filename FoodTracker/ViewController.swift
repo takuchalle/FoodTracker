23b6b2e037b59d34ba49793e47fe29080cb8b8aa
Takuya OHASHI <tky.ohashi@gmail.com>
2016-02-25 08:40:28 +0900

    implement the selectImageFromPhotoLibrary(_:) action method

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -36,6 +36,17 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
     // MARK: Actions
     
     @IBAction func selectImageFromPhotoLibrary(sender: UITapGestureRecognizer) {
+        // Hide the keyboard
+        nameTextField.resignFirstResponder()
+        // UIImagePickerController is a view controller that lets a user pick media from their photo library.
+        let imagePickerController = UIImagePickerController()
+        // Only allow photos to be picked, not taken.
+        imagePickerController.sourceType = .PhotoLibrary
+        // Make sure ViewController is notified when the user picks an image.
+        imagePickerController.delegate = self
+        
+        presentViewController(imagePickerController, animated: true, completion: nil)
+        
     }
     @IBAction func setDefaultLabelText(sender: UIButton) {
         mealNameLabel.text = "Default Text"
