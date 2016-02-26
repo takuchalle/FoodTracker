99ca564a76808d3bf45756ad8c96a830025ddcbd
Takuya OHASHI <tky.ohashi@gmail.com>
2016-02-26 23:56:27 +0900

    create a subclass of UIView

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,15 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by takuya on 2016/02/26.
+//  Copyright © 2016年 Takuya OHASHI. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIView {
+
+
+
+}
