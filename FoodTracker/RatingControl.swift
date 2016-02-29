871cc73c29da3198fcc57dd5cba2358010bb3644
Takuya OHASHI <tky.ohashi@gmail.com>
2016-02-29 23:46:26 +0900

    create a button in your view

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -13,6 +13,12 @@ class RatingControl: UIView {
     // MARK: Initialization
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
+        button.backgroundColor = UIColor.redColor()
+        addSubview(button)
     }
 
+    override func intrinsicContentSize() -> CGSize {
+        return CGSize(width: 240, height: 44)
+    }
 }
