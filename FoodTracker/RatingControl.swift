4e84cdf77fb306e37978de12fce38423b36a399d
Takuya OHASHI <tky.ohashi@gmail.com>
2016-03-01 00:07:02 +0900

    add an action to the button

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -15,10 +15,15 @@ class RatingControl: UIView {
         super.init(coder: aDecoder)
         let button = UIButton(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
         button.backgroundColor = UIColor.redColor()
+        button.addTarget(self, action: "ratingButtonTapped:", forControlEvents: .TouchDown)
         addSubview(button)
     }
 
     override func intrinsicContentSize() -> CGSize {
         return CGSize(width: 240, height: 44)
+    }
+    // MARK: Button Action
+    func ratingButtonTapped(button: UIButton) {
+        print("Button pressed 👍")
     }
 }
