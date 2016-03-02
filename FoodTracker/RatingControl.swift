677998258f0f3499316b267593cbc68b440ae5c0
Takuya OHASHI <tky.ohashi@gmail.com>
2016-03-02 23:14:57 +0900

    create a total of five buttons

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -16,10 +16,13 @@ class RatingControl: UIView {
     // MARK: Initialization
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
-        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
-        button.backgroundColor = UIColor.redColor()
-        button.addTarget(self, action: "ratingButtonTapped:", forControlEvents: .TouchDown)
-        addSubview(button)
+        for _ in 0..<5 {
+            let button = UIButton(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
+            button.backgroundColor = UIColor.redColor()
+            button.addTarget(self, action: "ratingButtonTapped:", forControlEvents: .TouchDown)
+            ratingButtons += [button]
+            addSubview(button)
+        }
     }
 
     override func intrinsicContentSize() -> CGSize {
