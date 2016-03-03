5ca60a4bcaf81d9f37425c2f66bc2e57d245c1ee
Takuya OHASHI <tky.ohashi@gmail.com>
2016-03-03 22:34:45 +0900

    lay out the buttons

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -25,6 +25,14 @@ class RatingControl: UIView {
         }
     }
 
+    override func layoutSubviews() {
+        var buttonFrame = CGRect(x: 0, y: 0, width: 44, height: 44)
+        for (index, button) in ratingButtons.enumerate() {
+            buttonFrame.origin.x = CGFloat(index * (44 + 5))
+            button.frame = buttonFrame
+        }
+    }
+
     override func intrinsicContentSize() -> CGSize {
         return CGSize(width: 240, height: 44)
     }
