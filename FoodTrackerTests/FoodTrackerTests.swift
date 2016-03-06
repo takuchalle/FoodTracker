c238b89878350b5d8d72e875cc9ccf972e6f9286
Takuya OHASHI <tky.ohashi@gmail.com>
2016-03-06 14:01:15 +0900

    write a unit test for Meal object initialization

--- FoodTrackerTests/FoodTrackerTests.swift
+++ FoodTrackerTests/FoodTrackerTests.swift
@@ -10,27 +10,16 @@ import XCTest
 @testable import FoodTracker
 
 class FoodTrackerTests: XCTestCase {
+    // MARK: FoodTracker Tests
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    // Tests to confirm that the Meal initializer returns when no name or a negative rating is provided.
+    func testMealInitialization() {
+        let potentialItem = Meal(name: "NewestMeal", photo: nil, rating: 5)
+        XCTAssertNotNil(potentialItem)
+        // Failure Case
+        let noName = Meal(name: "", photo: nil, rating: 0)
+        XCTAssertNil(noName, "Empty name is invalid")
+        let badRating = Meal(name: "Really bad rating", photo: nil, rating: -1)
+        XCTAssertNotNil(badRating)
     }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
