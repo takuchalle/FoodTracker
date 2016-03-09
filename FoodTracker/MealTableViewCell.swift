cb05a7001d0eee2d063762ff367d43e7f8d30808
Takuya OHASHI <tky.ohashi@gmail.com>
2016-03-09 23:18:48 +0900

    create a subclass of UITableViewCell

---
+++ FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by takuya on 2016/03/09.
+//  Copyright © 2016年 Takuya OHASHI. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
