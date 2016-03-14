efa5783a2b1242f617cd804981f8207cb6ad0dcb
Takuya OHASHI <tky.ohashi@gmail.com>
2016-03-14 23:47:48 +0900

    connect the views to the MealTableViewCell.swift code

--- FoodTracker/MealTableViewCell.swift
+++ FoodTracker/MealTableViewCell.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class MealTableViewCell: UITableViewCell {
+    // MARK: Properties
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
 
     override func awakeFromNib() {
         super.awakeFromNib()
