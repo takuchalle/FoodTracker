f3bd2dd84996dd20dc89b4523f1606b17c6a46e5
Takuya OHASHI <tky.ohashi@gmail.com>
2016-03-16 23:55:12 +0900

    clean up unused pieces of the project

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -13,7 +13,6 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
     // MARK: Properties
 
     @IBOutlet weak var nameTextField: UITextField!
-    @IBOutlet weak var mealNameLabel: UILabel!
     @IBOutlet weak var photoImageView: UIImageView!
     @IBOutlet weak var ratingControl: RatingControl!
     override func viewDidLoad() {
@@ -31,7 +30,7 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
     }
 
     func textFieldDidEndEditing(textField: UITextField) {
-        mealNameLabel.text = textField.text
+
     }
     
     // MARK: Actions
