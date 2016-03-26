732def316d33ae1da962566a436683e516e8c33a
Takuya OHASHI <tky.ohashi@gmail.com>
2016-03-26 14:38:23 +0900

    update the implementation of viewDidLoad

--- FoodTracker/MealViewController.swift
+++ FoodTracker/MealViewController.swift
@@ -29,6 +29,13 @@ class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerCo
         // Handle the text field’s user input through delegate callbacks.
         nameTextField.delegate = self
 
+        if let meal = meal {
+            navigationItem.title = meal.name
+            nameTextField.text   = meal.name
+            photoImageView.image = meal.photo
+            ratingControl.rating = meal.rating
+        }
+
         // Enable the Save button only if the text field has a valid Meal name.
         checkValidMealName()
     }
